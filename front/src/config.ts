b0bdcc5364676944e119f4a311cb173d5dc2c8a8
Sohei Yamaga <me@eliza.link>
2023-07-06 15:50:57 +0900

    バックエンドのURLを開発と本番で切り替わるようにした

--- front/src/config.ts
+++ front/src/config.ts
@@ -1,4 +1,5 @@
-export const api_root: string = 'http://localhost:8080/'
+// export const api_root: string = 'http://localhost:8080/'
+export const api_root: string = import.meta.env.VITE_BACKEND_URL
 export const api_article_root: string = api_root + 'article/'
 export const api_plain_article_root: string = api_root + 'plain_article/'
 export const api_static_root: string = api_root + 'static/'
