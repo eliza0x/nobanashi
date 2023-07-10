d87f7671155fe7058186d679c009bfd17403f3f1
Sohei Yamaga <me@eliza.link>
2023-07-10 17:14:49 +0900

    docker run時にstack buildが走らないように調整

--- backend/run.sh
+++ backend/run.sh
@@ -1,9 +1,10 @@
 set -e
 
 echo "==== build ===="
-# docker build --platform linux/x86_64 --progress=plain --rm -t nobanashi-backend .
+# docker build --platform linux/amd64 --progress=plain --rm -t nobanashi-backend .
 docker build --progress=plain --rm -t nobanashi-backend .
 
 echo "==== run ===="
 export PORT=8080
+# docker run -it --platform linux/amd64 --env NOBANASHI_PASS=pass --env PORT=$PORT --expose $PORT nobanashi-backend
 docker run -it --env NOBANASHI_PASS=pass --env PORT=$PORT --expose $PORT nobanashi-backend
